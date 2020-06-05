e0d305c87a0abc718f2961b3003acd758f43d27d
panmingjie <670220975@qq.com>
2020-06-05 08:14:32 +0800

    增加volatile

---
+++ src/main/java/com/mjie/synchronize/Cache1.java
@@ -0,0 +1,61 @@
+package com.mjie.synchronize;
+
+import sun.misc.Contended;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Cache1 {
+
+    private static long count = 100000000L;
+
+    /**
+     * 保证和其他实例的成员变量不在同一行
+     */
+    @Contended
+    private static class T {
+        public volatile long x = 0L;
+    }
+
+    public static T[] arr = new T[2];
+    static {
+        arr[0] = new T();
+        arr[1] = new T();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                arr[0].x = i;
+            }
+            countDownLatch.countDown();
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                arr[1].x = i;
+            }
+            countDownLatch.countDown();
+        });
+
+
+        long timeMillis = System.nanoTime();
+        t1.start();
+        t2.start();
+        countDownLatch.await();
+        System.out.println("执行耗时 " + (System.nanoTime() - timeMillis));
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/mjie/synchronize/Cache2.java
@@ -0,0 +1,62 @@
+package com.mjie.synchronize;
+
+import sun.misc.Contended;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Cache2 {
+
+    private static long count = 100000000L;
+
+    /**
+     * 保证和其他实例的成员变量不在同一行
+     */
+    private static class T {
+        public volatile long l1,l2,l3,l4,l5,l6,l7,l8 = 0L;
+        public volatile long x = 0L;
+        public volatile long l9,l10,l11,l12,l13,l14,l15 = 0L;
+    }
+
+    public static T[] arr = new T[2];
+    static {
+        arr[0] = new T();
+        arr[1] = new T();
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                arr[0].x = i;
+            }
+            countDownLatch.countDown();
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < count; i++) {
+                arr[1].x = i;
+            }
+            countDownLatch.countDown();
+        });
+
+
+        long timeMillis = System.nanoTime();
+        t1.start();
+        t2.start();
+        countDownLatch.await();
+        System.out.println("执行耗时 " + (System.nanoTime() - timeMillis));
+
+
+
+
+    }
+
+
+
+
+
+
+}
